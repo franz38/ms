ea3df64e951bd5b08a1ca097efdb8e2d10cddf84
franz38 <mattefranzz@gmail.com>
2021-10-25 17:24:22 +0200

    working yt

--- md.py
+++ md.py
@@ -10,236 +10,107 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
+from cmanager import Cmanager
 
-class myPL:
 
-    def __init__(self, string):
-        tmp = string.split(" ")
-        self.url = tmp[0]
-        self.songs = tmp[1]
-        self.title = (' '.join(tmp[2:])).strip()
-
-    def __str__(self):
-        return self.title + " " + self.songs
-
-class SongPlaceholder:
-
-    def __init__(self, string):
-        tmp = string.split(" ")
-        self.url = tmp[0]
-        self.title = (' '.join(tmp[1:])).strip()
-
-    def __str__(self):
-        return self.url + " " + self.title
-
-def read_list(list_file_path):
-
-    file = open(list_file_path, "r")
-    lines = file.readlines()
-    items = []
-
-    for string in lines:
-        string = string.strip()
-        if string != "" and string != " ":
-            items.append( SongPlaceholder(string) )
-
-    return items
-
-def write_playlist_list(list_file_path, playlist_list): #url title/folder_name
-
-    file = open(list_file_path, "w")
-    for pl in playlist_list:
-        file.write(pl.url + " " + pl.title + "\n")
-    file.close()
-
-def in_items(items, url=None, title=None):
-
-    if url != None:
-        for item in items:
-            if url == item.url:
-                return True
-        return False
-
-    elif title!=None:
-        for item in items:
-            if title == item.title:
-                return True
-        return False
 
 def dl_audio(video_url, location):
     yt = YouTube(video_url)
     y_audio = yt.streams.get_audio_only()
 
+    minutes = int(yt.length/60)
+    seconds = int(yt.length%60)
+
     filename = y_audio.default_filename
     file_path = location + "/" + filename
 
     if os.path.isfile(file_path):
-        print(filename + " found")
-        return ("found", filename)
+        print("# " + filename + " [found]")
+        return ("found", filename, minutes, seconds)
     else:
-        print(filename + " downloaded")
+        print("# " + filename + " [downloaded]")
         y_audio.download(location)
-        return ("downloaded", filename)
-
-# def dl_audio_list(video_url_list, location):
-#     for url in video_url_list:
-#         status, filename = dl_audio(url, location)
-
-def download_playlist(playlist_url, dump_location=""):
-
-    playlist = Playlist(playlist_url)
-
-    if not playlist or not playlist.title:
-        print("Url not valid")
-        return None
-
-    p_name = playlist.title
-    location = dump_location + p_name
-
-    try:
-        os.mkdir(location)
-    except OSError as error:
-        pass
-        # print("directory already exists")
-
-    if not os.path.isfile(location + '/config'):
-        config = open(location + '/config', 'w+')
-        config.close()
-
-    config = open(location + '/config', 'r')
-    old_songs_strings = config.readlines()
-    old_songs = []
-    for string in old_songs_strings:
-        string = string.strip()
-        if string != "" and string != " ":
-            old_songs.append( SongPlaceholder(string) )
+        return ("downloaded", filename, minutes, seconds)
 
 
-    config.close()
-    new_songs_url = []
+def dl_audio_list(pl, video_url_list, folder):
 
-    songs = {
-        "already_synchronized":0,
-        "found":0,
-        "downloaded":0,
-        "deleted": 0
-    }
+    to_del = pl.remove_deleted(video_url_list)
+    for filename in to_del:
+        print(filename)
+        try:
+            os.remove(folder + "/" + filename)
+        except OSError as error:
+            print("file to delete not found")
 
-
-    #check videos removed from playlist
-    for song in list(old_songs):
-        if not song.url in playlist.video_urls:
-            #print("deleting " + str(song))
-            songs["deleted"] += 1
-            old_songs.remove(song)
-            try:
-                os.remove(location + "/" + song.title)
-            except OSError as error:
-                print("file to delete not found")
-
-
-    for url in playlist.video_urls:
-
-        found = False
-        for oldsong in old_songs:
-            if url == oldsong.url:
-                found = True
-
-        if found: #already synchronized
-            songs["already_synchronized"] += 1
+    for url in video_url_list:
+        if pl.has_song(url):
+            pass
         else:
-            status, filename = dl_audio(url, location)
-            songs[status] += 1
-            new_songs_url.append( SongPlaceholder(url + " " + filename))
-
-    config = open(location + '/config', 'w')
-    for song in old_songs:
-        config.write(str(song) + "\n")
-    for song in new_songs_url:
-        config.write(str(song) + "\n")
-    config.close()
-
-    print("\n# '" + p_name + "' synchronized")
-    print(" - already synchronized \t" + str(songs["already_synchronized"]))
-    print(" - found \t\t\t" + str(songs["found"]))
-    print(" - downloaded \t\t\t" + str(songs["downloaded"]))
-    print(" - deleted \t\t\t" + str(songs["deleted"]))
-    return p_name
+            status, filename, minutes, seconds = dl_audio(url, folder)
+            pl.add_song(url, filename, minutes, seconds)
 
 
-def add_playlist(playlist_url, dump_location=""):
+def add_playlist(playlist_url):
 
-    if not os.path.isfile(dump_location + 'config'):
-        config = open(dump_location + 'config', 'w+')
-        config.close()
+    if cmanager.get_pl(playlist_url) == None:
+        if True: #yt
+            playlist = Playlist(playlist_url)
 
-    playlists = read_list(dump_location + 'config')
+            if not playlist or not playlist.title:
+                print("Url not valid")
+                return None
 
-    if not in_items(playlists, playlist_url):
-        config = open(dump_location + 'config', 'a')
-        playlist_name = download_playlist(playlist_url)
-        if playlist_name:
-            config.write(playlist_url + " " + playlist_name + "\n")
-            print("Playlist added")
-        else:
-            print("Playlist not added")
+            p_name = playlist.title
+            yt_url_list = playlist.video_urls
+        else: #sp
+            pass
+
+        aa = cmanager.add_pl(playlist_url, p_name)
+
+        folder = DUMP_LOCATION + p_name
+
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+
+        print("\n# '" + p_name)
+        dl_audio_list(aa, yt_url_list, folder)
+        aa.print_synch_info()
+
     else:
         print("Playlist already added")
 
 
+def remove_playlist(playlist_id):
 
+    pl = cmanager.remove_pl(playlist_id)
 
+    if os.path.isfile(DUMP_LOCATION + pl.title):
+        shutil.rmtree(DUMP_LOCATION + pl.title)
 
-def remove_playlist(playlist_id, dump_location=""):
-    playlists = read_list(dump_location + 'config')
-    pl = playlists.pop(playlist_id)
-
-    if os.path.isfile(dump_location + pl.title + "/config"):
-        shutil.rmtree(dump_location + pl.title)
-        write_playlist_list(dump_location + "config", playlists)
-        print("Playlist deleted succesfully at " + dump_location + pl.title + "/")
-    else:
-        print("config file not found :(")
-
-
-def sync(dump_location=""):
-
-    playlists = []
-    with open(dump_location + 'config', 'r') as config:
-        lines = config.readlines()
-        playlists = [SongPlaceholder(line.rstrip()) for line in lines]
-
-    print("### " + str(len(playlists)) + " Playlists found ###")
-
-    for playlist in playlists:
-        download_playlist(playlist.url)
-
-
-def about(dump_location=""):
-    playlists = read_list(dump_location + 'config')
-
-    print("### " + str(len(playlists)) + " Playlists found ###\n")
-
-    i=1
-    for pl in playlists:
-        print("# " + '{0:03}'.format(i) + " " + pl.title)
-        i+=1
-
-    print("")
+    print(pl.title + " removed")
 
 
 def print_help():
     print("Commands:")
     print("\tsync \tsync all the stuff")
     print("\tadd_pl \tadd Playlist")
+    print("\--status \tadd Show all")
+    print("\--list \tadd List the playlists addded")
+
+
+
+DUMP_LOCATION = ""
 
 param = sys.argv[1:]
+cmanager = Cmanager("config")
+
+if not os.path.isfile("" + 'config'):
+    config = open("" + 'config', 'w+')
+    config.close()
 
 if param == [] or param == None or param == ["--help"] or param == ["-h"]:
     print_help()
-
-elif param[0] == "sync":
-    sync()
 
 elif param[0] == "add_pl":
     if len(param) == 2:
@@ -247,8 +118,11 @@ elif param[0] == "add_pl":
     else:
         print("wrong parameters number")
 
-elif param[0] == "about":
-    about()
+elif param[0] == "--list":
+    print(cmanager.playlists_list())
+
+elif param[0] == "--status":
+    print(cmanager)
 
 elif param[0] == "rm_pl":
     if len(param) == 2:
@@ -264,3 +138,6 @@ elif param[0] == "add_spotify_pl":
 
 else:
     print("Parameters not catched")
+
+
+cmanager.write_back()
